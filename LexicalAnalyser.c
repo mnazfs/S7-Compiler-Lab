d30e37a7beaddf66e2b49bf058621295f738e55e
mnazfs <145181561+mnazfs@users.noreply.github.com>
2025-07-14 22:44:57 +0530

    Update LexicalAnalyser.c

--- LexicalAnalyser.c
+++ LexicalAnalyser.c
@@ -18,7 +18,7 @@ void main(int argc, char* argv[])
 	fp1 = fopen(argv[1], "r");
 	fp2 = fopen(argv[2], "w");
 	
-	printf("Sl No\tLexeme\tToken\t\tLine No\n-----------------------------------------------\n");
+	printf("Sl No\t\tLexeme\t\tToken\t\tLine No\n-----------------------------------------------\n");
 	
 	while((ch = fgetc(fp1)) != EOF) {
 		if(ch == ' '||ch == '\t');
@@ -27,35 +27,35 @@ void main(int argc, char* argv[])
 			line++;	
 			
 		else if(ch == ';') {
-			printf("%d\t%c\tSemi-colon\t\t%d\n", slno, ch, line);
+			printf("%d\t\t%c\t\tSemi-colon\t\t%d\n", slno, ch, line);
 			slno++;
 		}
 		
 		else if(ch == '+'||ch == '-'||ch == '*'||ch == '/') {
-			printf("%d\t%c\tArith_op\t\t%d\n", slno, ch, line);
+			printf("%d\t\t%c\t\tArith_op\t\t%d\n", slno, ch, line);
 			slno++;
 		}
 		
 		else if(ch == '{'||ch == '['||ch == '(') {
-			printf("%d\t%c\tOpen_bracket\t\t%d\n", slno, ch, line);
+			printf("%d\t\t%c\t\tOpen_bracket\t\t%d\n", slno, ch, line);
 			slno++;
 		}
 		
 		else if(ch == '}'||ch == ']'||ch == ')') {
-			printf("%d\t%c\tClose_bracket\t\t%d\n", slno, ch, line);
+			printf("%d\t\t%c\t\tClose_bracket\t\t%d\n", slno, ch, line);
 			slno++;
 		}
 		
 		else if(ch == '&'||ch == ':'||ch == ','||ch == '#') {
-			printf("%d\t%c\tSpecial_char\t\t%d\n", slno, ch, line);
+			printf("%d\t\t%c\t\tSpecial_char\t\t%d\n", slno, ch, line);
 			slno++;
 		}
 		
 		else if(ch == '=') {
 			if((ch1 = fgetc(fp1)) == '=')
-				printf("%d\t%c%c\tRelat_op\t\t%d\n", slno, ch, ch1, line);
+				printf("%d\t\t%c%c\t\tRelat_op\t\t%d\n", slno, ch, ch1, line);
 			else {
-				printf("%d\t%c\tAssign_op\t\t%d\n", slno, ch, line);
+				printf("%d\t\t%c\t\tAssign_op\t\t%d\n", slno, ch, line);
 				ungetc(ch1, fp1);
 			}
 			slno++;
@@ -63,9 +63,9 @@ void main(int argc, char* argv[])
 		
 		else if(ch == '<'||ch == '>'||ch == '!') {
 			if((ch1 = fgetc(fp1)) == '=')
-				printf("%d\t%c%c\tRelat_op\t\t%d\n", slno, ch, ch1, line);
+				printf("%d\t\t%c%c\t\tRelat_op\t\t%d\n", slno, ch, ch1, line);
 			else {
-				printf("%d\t%c\tRelat_op\t\t%d\n", slno, ch, line);
+				printf("%d\t\t%c\t\tRelat_op\t\t%d\n", slno, ch, line);
 				ungetc(ch1, fp1);
 			}
 			slno++;
@@ -87,9 +87,9 @@ void main(int argc, char* argv[])
 			ungetc(ch1, fp1);
 			
 			if(flag)
-				printf("%d\t%s\tFloat_point\t\t%d\n", slno, lexeme, line);
+				printf("%d\t\t%s\t\tFloat_point\t\t%d\n", slno, lexeme, line);
 			else
-				printf("%d\t%s\tNumber\t\t%d\n", slno, lexeme, line);
+				printf("%d\t\t%s\t\tNumber\t\t%d\n", slno, lexeme, line);
 			slno++;
 			memset(lexeme, '\0', sizeof(lexeme));
 		}
@@ -114,15 +114,15 @@ void main(int argc, char* argv[])
 				}
 				
 			if(flag)
-				printf("%d\t%s\tKeyword   \t\t%d\n", slno, lexeme, line);
+				printf("%d\t\t%s\t\tKeyword\t\t%d\n", slno, lexeme, line);
 			else
-				printf("%d\t%s\tIdentifier\t\t%d\n", slno, lexeme, line);
+				printf("%d\t\t%s\t\tIdentifier\t\t%d\n", slno, lexeme, line);
 			slno++;
 			memset(lexeme, '\0', sizeof(lexeme));
 		}
 		
 		else {
-			printf("%d\t%c\tUnrecognized\t\t%d\n", slno, ch, line);
+			printf("%d\t\t%c\t\tUnrecognized\t\t%d\n", slno, ch, line);
 			slno++;
 		}
 	}
